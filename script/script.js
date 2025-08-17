393ca0854b3a6fd63fb30024604ffda15ef2c878
Mirembe Peace Mercy <peace2de@gmail.com>
2025-08-17 21:08:34 +0300

    basic setup

---
+++ script/script.js
@@ -0,0 +1,9 @@
+import * as THREE from THREE;
+import {orbitControls} from 'three/addons/controls/orbitControls';
+import {RGBELoader} from 'three/addons/loaders/RGBELoader';
+
+//scene
+//camera
+//lights
+//loading HDR
+//animation Loop
